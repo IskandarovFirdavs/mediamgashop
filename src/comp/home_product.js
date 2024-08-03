f1d035903adb7ad2536249a1cb988d30d573cb9b
IskandarovFirdavs <145989337+IskandarovFirdavs@users.noreply.github.com>
2024-08-03 22:45:14 +0500

    coommit 1

---
+++ src/comp/home_product.js
@@ -0,0 +1,111 @@
+import Img1 from "../image/p1.webp";
+import Img2 from "../image/p2.webp";
+import Img3 from "../image/p3.webp";
+import Img4 from "../image/p4.webp";
+import Img5 from "../image/p5.webp";
+import Img6 from "../image/p6.webp";
+import Img7 from "../image/p7.webp";
+import Img8 from "../image/p8.webp";
+import Img9 from "../image/p9.jpg";
+import Img10 from "../image/p10.jpg";
+import Img11 from "../image/p11.jpg";
+import Img12 from "../image/p12.jpg";
+const Homeproduct = [
+  {
+    id: 1,
+    Name: "product1",
+    price: "26",
+    image: Img1,
+    cat: "tv",
+    type: "new"
+  },
+  {
+    id: 2,
+    Name: "product2",
+    price: "96",
+    image: Img2,
+    cat: "laptop",
+    type: "new"
+  },
+  {
+    id: 3,
+    Name: "product3",
+    price: "236",
+    image: Img3,
+    cat: "watch",
+    type: "new"
+  },
+  {
+    id: 4,
+    Name: "product4",
+    price: "636",
+    image: Img9,
+    cat: "laptop",
+    type: "new"
+  },
+  {
+    id: 5,
+    Name: "product5",
+    price: "98",
+    image: Img4,
+    cat: "speaker",
+    type: "featured"
+  },
+  {
+    id: 6,
+    Name: "product6",
+    price: "59",
+    image: Img5,
+    cat: "electronics",
+    type: "featured"
+  },
+  {
+    id: 7,
+    Name: "product7",
+    price: "956",
+    image: Img10,
+    cat: "headphone",
+    type: "featured"
+  },
+  {
+    id: 8,
+    Name: "product8",
+    price: "389",
+    image: Img11,
+    cat: "electronics",
+    type: "featured"
+  },
+  {
+    id: 9,
+    Name: "product9",
+    price: "67",
+    image: Img6,
+    cat: "phone",
+    type: "top"
+  },
+  {
+    id: 10,
+    Name: "product10",
+    price: "76",
+    image: Img7,
+    cat: "speaker",
+    type: "top"
+  },
+  {
+    id: 11,
+    Name: "product11",
+    price: "87",
+    image: Img8,
+    cat: "speaker",
+    type: "top"
+  },
+  {
+    id: 12,
+    Name: "product12",
+    price: "807",
+    image: Img12,
+    cat: "speaker",
+    type: "top"
+  }
+];
+export default Homeproduct;
